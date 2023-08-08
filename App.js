2e1fdee8080e9953f6c86988aebc3a47c2aed003
carinaavram <carinaavram97@gmail.com>
2023-08-08 14:56:20 -0400

    first and final commit

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useCallback } from 'react';
+import {
+  StyleSheet,
+  ImageBackground,
+  SafeAreaView,
+  ActivityIndicator,
+  StatusBar,
+} from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOverScreen';
+import Colors from './constants/colors';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState(null);
+  const [gameIsOver, setGameIsOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+    setGameIsOver(false);
+  }
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
+  let screen = <StartGameScreen onPickedNumber={pickedNumberHandler} />;
+
+  function gameOverHandler(numberOfRounds) {
+    setGameIsOver(true);
+    setGuessRounds(numberOfRounds);
+  }
+
+  if (userNumber) {
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
+  }
+
+  if (gameIsOver && userNumber) {
+    screen = (
+      <GameOverScreen
+        userNumber={userNumber}
+        roundsNumber={guessRounds}
+        onStartNewGame={startNewGameHandler}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
+        style={styles.appContainer}
+        onLayout={onLayoutRootView}
+      >
+        <StatusBar barStyle='light-content' translucent={true} />
+        <ImageBackground
+          source={require('./assets/images/background.png')}
+          resizeMode="cover"
+          style={styles.appContainer}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.appContainer}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  backgroundImage: {
+    opacity: 0.2,
   },
 });
---
+++ README.md
@@ -0,0 +1,20 @@
+# Guessing Game App
+
+## Description
+
+The Guessing Game App is a mini game where the user thinks of a number and the phone tries to guess it. The user provides hints (+ or -) to guide the phone's guesses. Each round of guesses is displayed on the screen along with the round number and the phone's guess. The game continues until the phone successfully guesses the user's number. At the end of the game, the app reveals how many rounds were needed and displays the user's chosen number.
+
+This app was built using React Native and Expo.
+
+## Features
+
+- User chooses a secret number.
+- Phone makes guesses and adjusts them based on user feedback.
+- Each round is displayed with round number and phone's guess.
+- Game ends when the phone successfully guesses the user's number.
+- Game statistics are displayed at the end.
+
+## Technologies Used
+
+- React Native
+- Expo
